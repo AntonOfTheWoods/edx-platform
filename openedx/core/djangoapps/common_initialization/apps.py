a3c96593f6d7614bf824d59bd4a9fa5245955155
Anton Melser <anton@melser.org>
2025-10-08 13:00:54 +0800

    Add proper initiliser for opaque keys
    Signed-off-by: Anton Melser <anton.melser@outlook.com>

--- openedx/core/djangoapps/common_initialization/apps.py
+++ openedx/core/djangoapps/common_initialization/apps.py
@@ -13,6 +13,7 @@ class CommonInitializationConfig(AppConfig):  # lint-amnesty, pylint: disable=mi
         # Common settings validations for the LMS and CMS.
         from . import checks  # lint-amnesty, pylint: disable=unused-import
         self._add_mimetypes()
+        self._register_psycopg_adapters()
 
     @staticmethod
     def _add_mimetypes():
@@ -25,3 +26,43 @@ class CommonInitializationConfig(AppConfig):  # lint-amnesty, pylint: disable=mi
         mimetypes.add_type('application/x-font-opentype', '.otf')
         mimetypes.add_type('application/x-font-ttf', '.ttf')
         mimetypes.add_type('application/font-woff', '.woff')
+
+    @staticmethod
+    def _register_psycopg_adapters():
+        """
+        Register psycopg3 adapters for opaque_keys types when using PostgreSQL.
+
+        This allows CourseLocator, LibraryLocator, and other opaque key objects
+        to be passed directly to Django queries without manual string conversion.
+        """
+        from django.conf import settings
+
+        # Only register adapters if we're using PostgreSQL
+        databases_using_postgres = [
+            db_config for db_config in settings.DATABASES.values()
+            if 'postgresql' in db_config.get('ENGINE', '').lower() or
+               'psycopg' in db_config.get('ENGINE', '').lower()
+        ]
+
+        if databases_using_postgres:
+            try:
+                from psycopg.adapt import Dumper
+                from psycopg.adapters import AdaptersMap
+                from opaque_keys.edx.keys import OpaqueKey
+
+                class OpaqueKeyDumper(Dumper):
+                    """
+                    Custom psycopg3 dumper for OpaqueKey subclasses.
+                    Converts opaque keys to their string representation for database storage.
+                    """
+                    def dump(self, obj):
+                        return str(obj).encode('utf-8')
+
+                # Register the dumper for OpaqueKey and its subclasses
+                # This will handle CourseLocator, LibraryLocator, UsageKey, etc.
+                adapters = AdaptersMap.get_global()
+                adapters.register_dumper(OpaqueKey, OpaqueKeyDumper)
+
+            except ImportError:
+                # psycopg3 is not installed
+                pass
